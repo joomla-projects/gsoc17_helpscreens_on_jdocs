77d2054d6cf2aa1b45600afa4f0a87fbad0d9b8a
Shivam Rajput <shivamdiehard@gmail.com>
2017-06-19 20:59:19 +0530

    Guided Tour Update
    Updated the Target Class

--- media/plg_system_tour/js/tours.js
+++ media/plg_system_tour/js/tours.js
@@ -1,7 +1,7 @@
 (function ($) {
     $(document).ready(function() {
 
-        var btn= '<button class="btn btn-sm btn-outline-primary" id="startTourBtn"><span class="icon-trash"></span>Tour</button>';
+        var btn= '<button class="btn btn-sm btn-outline-primary" id="startTourBtn"><span class="icon"></span>Tour</button>';
         $('#toolbar').append(btn);
 
         var tour = {
@@ -9,36 +9,36 @@
                 steps: [
 
                     {
-                        target: document.querySelectorAll('[class="btn btn-sm btn-success"]')[0],
+                        target: document.querySelectorAll('[class="icon-new"]')[0],
                         title: 'Welcome to Hopscotch!',
                         content: 'Hey there! This is an example Hopscotch tour. There will be plenty of time to read documentation and sample code, but let\'s just take some time to see how Hopscotch actually works.',
-                        placement: 'right',
+                        placement: 'bottom',
                         arrowOffset: -1,
                         showCloseButton: true
                     },
                     {
-                        target: document.querySelectorAll('[class="btn btn-sm btn-outline-primary"]')[0],
+                        target: document.querySelectorAll('[class="icon-edit"]')[0],
                         title: 'Welcome to Hopscotch!',
                         content: 'Hey there! This is an example Hopscotch tour. There will be plenty of time to read documentation and sample code, but let\'s just take some time to see how Hopscotch actually works.',
                         placement: 'bottom',
                         arrowOffset: 0
                     },
                     {
-                        target: document.querySelectorAll('[class="btn btn-sm btn-outline-success"]')[0],
+                        target: document.querySelectorAll('[class="icon-publish"]')[0],
                         title: 'Welcome to Hopscotch!',
                         content: 'Hey there! This is an example Hopscotch tour. There will be plenty of time to read documentation and sample code, but let\'s just take some time to see how Hopscotch actually works.',
                         placement: 'bottom',
                         arrowOffset: 0
                     },
                     {
-                        target: document.querySelectorAll('[class="btn btn-sm btn-outline-danger"]')[0],
+                        target: document.querySelectorAll('[class="icon-unpublish"]')[0],
                         title: 'Welcome to Hopscotch!',
                         content: 'Hey there! This is an example Hopscotch tour. There will be plenty of time to read documentation and sample code, but let\'s just take some time to see how Hopscotch actually works.',
                         placement: 'bottom',
                         arrowOffset: 0
                     },
                     {
-                        target: document.querySelectorAll('[class="btn btn-sm btn-outline-primary"]')[4],
+                        target: document.querySelectorAll('[class="icon-trash"]')[0],
                         title: 'Welcome to Hopscotch!',
                         content: 'Hey there! This is an example Hopscotch tour. There will be plenty of time to read documentation and sample code, but let\'s just take some time to see how Hopscotch actually works.',
                         placement: 'bottom',
