7a03f009fb20ae168884979f697437497555a062
Shivam Rajput <shivamdiehard@gmail.com>
2017-06-22 02:29:00 +0530

    Tour Path
    Tour Path Update

--- media/plg_system_tour/js/tours.js
+++ media/plg_system_tour/js/tours.js
@@ -5,26 +5,30 @@
         var shiv = pathArray.split('&');
         var option= shiv[0].split("option=")[1];
         var view = shiv[1].split("view=")[1];
-        var currentURL = userLang + '/' + option;
-        if(shiv[2] != null) {
+        var currentURL = userLang + '/';
+        if(shiv[2] !=null) {
             var layout = shiv[2].split("layout=")[1];
         }
-        if (option != null && view != null && layout != null) {
+        if (option!=null && view !=null && layout !=null) {
             var filename =  option +'-'+ view + '-' + layout + '.' + 'json';
+            var URL = option + '/' + filename;
         }
-        if (option != null && view != null && layout == null) {
+        if (option!=null && view!=null && layout ==null) {
              filename =  option +'-'+ view +'.' + 'json';
-            var URL = currentURL + '/' + filename ;
+            URL = option + '/' + filename ;
         }
-        if( option !=null && view == null && layout == null)
+        if( option!=null && view==null && layout ==null )
         {
-            var filename =  option +  '.' + 'json';
+            filename =  option + '.' + 'json';
+            URL =  option +  '/' + filename;
         }
-        var currentURL = userLang + '/' + option + '/';
+        // var currentURL = userLang + '/' + option + '/';
         var btn= '<button class="btn btn-sm btn-outline-primary" id="startTourBtn"><span class="icon"></span>Tour</button>';
         $('#toolbar').append(btn);
         var myItems;
-        nURL = 'http://localhost/gsoc17_helpscreens_on_jdocs/media/guide/'+URL ;
+        var s = window.location.pathname.split('/');
+        nURL = window.location.protocol + '//' + window.location.host + '/' + s[1] + '/media/guide/' + navigator.language + '/'+ URL ;
+        console.log(nURL);
         $.getJSON( nURL  , function(data) {
             myItems = data.items;
             var tour = {
